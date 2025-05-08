734631276b0c4cfae936d080f135ea4875625e7c
Joy0-7 <23-53237-3@student.aiub.edu>
2025-05-09 02:03:45 +0600

    Joy
    frame 3,4

--- Fram 4/Form1.cs
+++ Fram 4/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Frame_3
+namespace Fram_4
 {
     public partial class Form1: Form
     {
@@ -17,7 +17,7 @@ namespace Frame_3
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
--- New pass fram/Form1.cs
+++ New pass fram/Form1.cs
@@ -8,18 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Frame_3
+namespace New_pass_fram
 {
     public partial class Form1: Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
