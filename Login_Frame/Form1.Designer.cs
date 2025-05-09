0ae38b44cd10916ce700ab9668df039538bea07c
Ahnaf Hasan Mehedi <161107950+Ahnaf0-0@users.noreply.github.com>
2025-05-10 00:17:01 +0600

    c

--- Login_Frame/Form1.Designer.cs
+++ Login_Frame/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.LogIn = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
@@ -44,9 +45,10 @@
             this.LogIn.Cursor = System.Windows.Forms.Cursors.Default;
             this.LogIn.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LogIn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.LogIn.Location = new System.Drawing.Point(119, 272);
+            this.LogIn.Location = new System.Drawing.Point(178, 418);
+            this.LogIn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.LogIn.Name = "LogIn";
-            this.LogIn.Size = new System.Drawing.Size(75, 32);
+            this.LogIn.Size = new System.Drawing.Size(112, 49);
             this.LogIn.TabIndex = 0;
             this.LogIn.Text = "Log In";
             this.LogIn.UseVisualStyleBackColor = false;
@@ -57,10 +59,11 @@
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.textBox1.Location = new System.Drawing.Point(31, 171);
+            this.textBox1.Location = new System.Drawing.Point(46, 263);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(259, 20);
+            this.textBox1.Size = new System.Drawing.Size(386, 29);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -70,9 +73,10 @@
             this.button1.Cursor = System.Windows.Forms.Cursors.Default;
             this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(91, 312);
+            this.button1.Location = new System.Drawing.Point(136, 480);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 35);
+            this.button1.Size = new System.Drawing.Size(190, 54);
             this.button1.TabIndex = 3;
             this.button1.Text = "Forget Password?";
             this.button1.UseVisualStyleBackColor = false;
@@ -81,9 +85,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Imprint MT Shadow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(28, 143);
+            this.label1.Location = new System.Drawing.Point(42, 220);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 14);
+            this.label1.Size = new System.Drawing.Size(45, 21);
             this.label1.TabIndex = 4;
             this.label1.Text = "ID :";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -92,9 +97,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Imprint MT Shadow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(28, 204);
+            this.label2.Location = new System.Drawing.Point(42, 314);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 14);
+            this.label2.Size = new System.Drawing.Size(139, 21);
             this.label2.TabIndex = 6;
             this.label2.Text = "PASSWORD :";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -102,9 +108,10 @@
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.SystemColors.Window;
-            this.textBox2.Location = new System.Drawing.Point(31, 231);
+            this.textBox2.Location = new System.Drawing.Point(46, 355);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(259, 20);
+            this.textBox2.Size = new System.Drawing.Size(386, 26);
             this.textBox2.TabIndex = 5;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
@@ -114,9 +121,10 @@
             this.button2.Cursor = System.Windows.Forms.Cursors.Default;
             this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Location = new System.Drawing.Point(107, 380);
+            this.button2.Location = new System.Drawing.Point(160, 585);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(99, 32);
+            this.button2.Size = new System.Drawing.Size(148, 49);
             this.button2.TabIndex = 7;
             this.button2.Text = "REGISTER";
             this.button2.UseVisualStyleBackColor = false;
@@ -127,19 +135,20 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Javanese Text", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(83, 354);
+            this.label3.Location = new System.Drawing.Point(124, 545);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(163, 29);
+            this.label3.Size = new System.Drawing.Size(242, 45);
             this.label3.TabIndex = 8;
             this.label3.Text = "Doesn\'t Have Account?";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(326, 428);
+            this.ClientSize = new System.Drawing.Size(489, 658);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -148,6 +157,8 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.LogIn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Log In Frame";
